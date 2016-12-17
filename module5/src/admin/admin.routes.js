f24fbaa0b0c30ef90404fc6558187a9869dc2efc
Scott T Cohen <scottcohen9999@gmail.com>
2016-12-17 15:10:48 -0500

    HW5 form appears on menu

--- module5/src/admin/admin.routes.js
+++ module5/src/admin/admin.routes.js
@@ -11,10 +11,10 @@ routeConfig.$inject = ['$stateProvider'];
 function routeConfig ($stateProvider) {
   // Routes
   $stateProvider
-    // .state('admin', {
-    //   abstract: true,
-    //   templateUrl: 'src/admin/admin.html'
-    // })
+    .state('admin', {
+      abstract: true,
+      templateUrl: 'src/admin/admin.html'
+    })
     // .state('admin.home', {
     //   url: '/',
     //   templateUrl: 'src/public/home/home.html'
