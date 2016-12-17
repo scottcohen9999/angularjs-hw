2c4fecef778cec98d775652cf938ca5236a44f6c
Scott T Cohen <scottcohen9999@gmail.com>
2016-12-17 15:10:48 -0500

    HW4 uses http to get data

--- module4/src/menuapp/menudata.service.js
+++ module4/src/menuapp/menudata.service.js
@@ -2,45 +2,45 @@
 'use strict';
 
 angular.module('MenuApp')
-.service('MenuDataService', MenuDataService);
+.service('MenuDataService', MenuDataService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 
-MenuDataService.$inject = ['$q', '$timeout']
-function MenuDataService($q, $timeout) {
+MenuDataService.$inject = ['$q', '$http','ApiBasePath']
+function MenuDataService($q, $http, ApiBasePath) {
   var service = this;
 
   // List of categories
   var categories = [];
 
-  // Pre-populate a categories list
-  categories.push({
-    name: "Sugar",
-    quantity: "2 bags",
-    description: "Sugar used for baking delicious umm... baked goods."
-  });
-  categories.push({
-    name: "flour",
-    quantity: "1 bags",
-    description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-  });
-  categories.push({
-    name: "Chocolate Chips",
-    quantity: "3 bags",
-    description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-  });
-
-  // Simulates call to server
   // Returns a promise, NOT categories array directly
   service.getAllCategories = function () {
+    var categoriesThusFar =[];
+    function contains(element1, index1, array1){
+      if(array1.length==0){
+        return 0;
+      }
+      return (array1[index1].indexOf(element1.toLowerCase()) === element1.length);
+    }
+    function isNewSubstring(element,index,array){
+      var doesContain = (categoriesThusFar.filter(contains).length>0);
+      if(!doesContain){
+        categoriesThusFar.push(element.short_name);
+      }
+
+      return doesContain;
+    }
     var deferred = $q.defer();
 
-    // Wait 2 seconds before returning
-    $timeout(function () {
-      // deferred.reject(categories);
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/categories.json")
+    }).then(function (response) {
+      response.data.filter(isNewSubstring);
+      categories=categoriesThusFar;
       deferred.resolve(categories);
-    }, 1800);
-
-    return deferred.promise;
+      return deferred.promise;
+    });
   };
 }
 
